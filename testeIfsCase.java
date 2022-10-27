39347e3f0d2d5e07c7de74cef444f08e5adbf3ce
Francisco Araújo <40128632+franciscoaraujo51@users.noreply.github.com>
2022-10-27 18:37:17 -0500

    start git to test cx flow

---
+++ testeIfsCase.java
@@ -0,0 +1,178 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.ArrayList;
+import java.util.*;
+
+
+class Teste {
+    public String teste1 = "primeiro";
+    public String teste2 = "segundo";
+    public String teste3 = "terceiro";
+    public String teste4 = "quarto";
+    public String teste5 = "quinto";
+    public String teste6 = "sexto";
+    public String teste7 = "setimo";
+    public String teste8 = "oitavo";
+    public String teste9 = "nono";
+    public String teste10 = "decimo";
+    public String teste11 = "onze";
+    public String teste12 = "doze";
+    public String teste13 = "treze";
+    public String teste14 = "catorze";
+    public String teste15 = "quinze";
+    public String teste16 = "dezasseis";
+    public String teste17 = "dezassete";
+    public String teste18 = "dezoito";
+    public String teste19 = "dezanove";
+    public String teste20 = "vinte";
+    public String teste21 = "vinteum";
+    public String teste22 = "vintedois";
+    public String teste23 = "vintetres";
+    public String teste24 = "vintequatro";
+    public String teste25 = "vintecinco";
+    public String teste26 = "vinteseis";
+    public String teste27 = "vintesete";
+    public String teste28 = "vinteoito";
+    public String teste29 = "vintenove";
+    public String teste30 = "trinta";
+    
+    public String getNumber1(){return teste1;}
+    public String getNumber2(){return teste2;}
+    public String getNumber3(){return teste3;}
+    public String getNumber4(){return teste4;}
+    public String getNumber5(){return teste5;}
+    public String getNumber6(){return teste6;}
+    public String getNumber7(){return teste7;}
+    public String getNumber8(){return teste8;}
+    public String getNumber9(){return teste9;}
+    public String getNumber10(){return teste10;}
+    public String getNumber11(){return teste11;}
+    public String getNumber12(){return teste12;}
+    public String getNumber13(){return teste13;}
+    public String getNumber14(){return teste14;}
+    public String getNumber15(){return teste15;}
+    public String getNumber16(){return teste16;}
+    public String getNumber17(){return teste17;}
+    public String getNumber18(){return teste18;}
+    public String getNumber19(){return teste19;}
+    public String getNumber20(){return teste20;}
+    public String getNumber21(){return teste21;}
+    public String getNumber22(){return teste22;}
+    public String getNumber23(){return teste23;}
+    public String getNumber24(){return teste24;}
+    public String getNumber25(){return teste25;}
+    public String getNumber26(){return teste26;}
+    public String getNumber27(){return teste27;}
+    public String getNumber28(){return teste28;}
+    public String getNumber29(){return teste29;}
+    public String getNumber30(){return teste30;}
+    
+    
+    
+    
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Teste t = new Teste();
+        
+        ArrayList al = new ArrayList<String>();
+       
+       if(t.getNumber1().equals("primeiro")){
+           al.add(t.getNumber1());
+       }
+       if(t.getNumber2().equals("segundo")){
+           al.add(t.getNumber2());
+       }
+       if(t.getNumber3().equals("terceiro")){
+           al.add(t.getNumber3());
+       }
+       if(t.getNumber4().equals("quarto")){
+           al.add(t.getNumber4());
+       }
+       if(t.getNumber5().equals("quinto")){
+           al.add(t.getNumber5());
+       }
+       if(t.getNumber6().equals("sexto")){
+           al.add(t.getNumber6());
+       }
+       if(t.getNumber7().equals("setimo")){
+           al.add(t.getNumber7());
+       }
+       if(t.getNumber8().equals("oitavo")){
+           al.add(t.getNumber8());
+       }
+       if(t.getNumber9().equals("nono")){
+           al.add(t.getNumber9());
+       }
+       if(t.getNumber10().equals("decimo")){
+           al.add(t.getNumber10());
+       }
+       if(t.getNumber11().equals("onze")){
+           al.add(t.getNumber11());
+       }
+       if(t.getNumber12().equals("doze")){
+           al.add(t.getNumber12());
+       }
+       if(t.getNumber13().equals("treze")){
+           al.add(t.getNumber13());
+       }
+       if(t.getNumber14().equals("catorze")){
+           al.add(t.getNumber14());
+       }
+       if(t.getNumber15().equals("quinze")){
+           al.add(t.getNumber15());
+       }
+       if(t.getNumber16().equals("dezasseis")){
+           al.add(t.getNumber16());
+       }
+       if(t.getNumber17().equals("dezassete")){
+           al.add(t.getNumber17());
+       }
+       if(t.getNumber18().equals("dezoito")){
+           al.add(t.getNumber18());
+       }
+       if(t.getNumber19().equals("dezanove")){
+           al.add(t.getNumber19());
+       }
+       if(t.getNumber20().equals("vinte")){
+           al.add(t.getNumber20());
+       }
+       if(t.getNumber21().equals("vinteum")){
+           al.add(t.getNumber21());
+       }
+       if(t.getNumber22().equals("vintedois")){
+           al.add(t.getNumber22());
+       }
+       if(t.getNumber23().equals("vintetres")){
+           al.add(t.getNumber23());
+       }
+       if(t.getNumber24().equals("vintequatro")){
+           al.add(t.getNumber24());
+       }
+       if(t.getNumber25().equals("vintecinco")){
+           al.add(t.getNumber25());
+       }
+       if(t.getNumber26().equals("vinteseis")){
+           al.add(t.getNumber26());
+       }
+       if(t.getNumber27().equals("vintesete")){
+           al.add(t.getNumber27());
+       }
+       if(t.getNumber28().equals("vinteoito")){
+           al.add(t.getNumber28());
+       }
+       if(t.getNumber29().equals("vintenove")){
+           al.add(t.getNumber29());
+       }
+        if(t.getNumber30().equals("trinta")){
+           al.add(t.getNumber30());
+       }
+       
+       al.forEach((n) -> System.out.println(n));
+       
+      
+    }   
+}
